fbfe02ad45c9d411718ad8dc4294348da4346be6
Lukinhas <lucasanimaplays17@gmail.com>
2025-05-18 17:57:07 -0300

    * Bot adicionado! * Melhoria da interação com a bola. * Correção do bug onde o botão de retornar na pausa não funcionava quando o primeiro estado do jogo é um modo de jogo.

--- game.py
+++ game.py
@@ -1,12 +1,12 @@
 import pygame, texts, buttons
 from sys import exit
-from random import choice, choices, randint
+from random import choice
 
 # PLAYER
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.Surface((10, 125))
+        self.image = pygame.Surface((10, 100))
         self.image.fill("white")
         self.rect = self.image.get_rect(center = (15, w_h/2))
         self.speed = 5
@@ -16,21 +16,23 @@ class Player(pygame.sprite.Sprite):
         ball = ballGroup.sprite
         if keys[pygame.K_w] or keys[pygame.K_UP]:
             self.rect.y -= self.speed
-            if ball.isColliding:
+            if ball.isCollidingPlayer:
                 ball.rect.y -= self.speed
         if keys[pygame.K_s] or keys[pygame.K_DOWN]:
             self.rect.y += self.speed
-            if ball.isColliding:
+            if ball.isCollidingPlayer:
                 ball.rect.y += self.speed
     
     def outofWIndow(self):
+        ball = ballGroup.sprite
         if self.rect.top < 10:
-            self.rect.top = 10
+            self.rect.top += self.speed
+            if ball.isCollidingPlayer:
+                ball.rect.y += self.speed
         if self.rect.bottom > w_h - 10:
-            self.rect.bottom = w_h - 10
-    
-    def returnRect(self):
-        return self.rect
+            self.rect.bottom -= self.speed
+            if ball.isCollidingPlayer:
+                ball.rect.y -= self.speed
     
     def update(self):
         self.movement()
@@ -44,40 +46,125 @@ class Ball(pygame.sprite.Sprite):
         self.speedX = 5
         self.speedY = 5
         self.playerHoldTimer = 0
-        self.isColliding = False
-        self.startDirectionX = choice([1, -1])
-        self.startDirectionY = choice([1, -1])
+        self.botHoldTimer = 0
+        self.collide = False
+        self.pointCollidedY = 0
+        self.isCollidingPlayer = False
+        self.isCollidingBot = False
+        self.speedMultiplierX = 1
+        self.speedMultiplierY = 1
+        self.DirectionX = choice([1, -1])
+        self.DirectionY = choice([1, -1])
     
     def movement(self):
-        self.rect.x += self.speedX * self.startDirectionX
-        if not(self.isColliding):
-            self.rect.y += self.speedY * self.startDirectionY
+        self.rect.x += self.speedX * self.DirectionX * self.speedMultiplierX
+        if not(self.isCollidingPlayer) and not(self.isCollidingBot):
+            self.rect.y += self.speedY * self.DirectionY * self.speedMultiplierY
     
     def collisions(self):
         if self.rect.top < 0 or self.rect.bottom > w_h:
-            self.speedY *= -1
+            if self.rect.top < 0:
+                self.rect.top = 0
+            if self.rect.bottom > w_h:
+                self.rect.bottom = w_h
+            self.DirectionY *= -1
+            self.speedMultiplierX += 0.01
+            self.speedMultiplierY += 0.012
         
         if self.rect.left < 0 or self.rect.right > w_w:
-            self.speedX *= -1
+            self.DirectionX *= -1
             
         player = playerGroup.sprite
+        bot = botGroup.sprite
         
         if self.rect.colliderect(player.rect):
-            self.isColliding = True
-            collision = self.rect.clip(player.rect)
+            if not(self.collide):
+                self.collide = True
+                self.pointCollidedY = self.rect.centery
+            self.isCollidingPlayer = True
+            collision_player = self.rect.clip(player.rect)
             self.playerHoldTimer += 1
-            self.rect.midleft = collision.midright
+            self.rect.midleft = collision_player.midright
             if self.playerHoldTimer == 6:       
                 self.speedX *= -1
-                print(self.playerHoldTimer)
                 self.playerHoldTimer = 0
+                self.speedMultiplierX += 0.015
+                self.speedMultiplierY += 0.012 
+                
         else:
-            self.isColliding = False
-            
+            self.isCollidingPlayer = False
+            if not(self.isCollidingBot):
+                self.collide = False
+        
+        if self.rect.colliderect(bot.rect):
+            if not(self.collide):
+                self.pointCollidedY = self.rect.centery
+                self.collide = True
+            self.isCollidingBot = True
+            collision_bot = self.rect.clip(bot.rect)
+            self.botHoldTimer += 1
+            self.rect.midright = collision_bot.midleft
+            if self.botHoldTimer == 6:       
+                self.speedX *= -1
+                self.botHoldTimer = 0
+                self.speedMultiplierX += 0.015
+                self.speedMultiplierY += 0.012
+        else:
+            self.isCollidingBot = False
+            if not(self.isCollidingPlayer):
+                self.collide = False
+    
+    def changeSide(self):
+        if self.isCollidingBot or self.isCollidingPlayer:
+            if self.rect.y < self.pointCollidedY:
+                self.DirectionY = -1
+            if self.rect.y > self.pointCollidedY:
+                self.DirectionY = 1
+    
     def update(self):
         self.movement()
         self.collisions()
+        self.changeSide()
+
+class Bot(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((10, 100))
+        self.image.fill("white")
+        self.rect = self.image.get_rect(center = (w_w-15, w_h/2)) 
+        self.speed = 5
+        self.side = choice([-1, 1])
         
+    def movement(self):
+        ball = ballGroup.sprite
+        if ball.rect.centery != self.rect.centery and not(ball.isCollidingBot):
+            if ball.rect.centery - self.rect.centery > 30:
+                self.rect.centery += self.speed
+
+            if ball.rect.centery - self.rect.centery < 30:
+                self.rect.centery -= self.speed
+
+        if ball.isCollidingBot:
+            self.rect.y += self.speed * self.side
+            ball.rect.y += self.speed * self.side
+        else:
+            self.side = choice([-1, 1])
+    
+    def outofWIndow(self):
+        ball = ballGroup.sprite
+        if self.rect.top < 10:
+            self.rect.top += self.speed
+            if ball.isCollidingBot:
+                ball.rect.y += self.speed
+        if self.rect.bottom > w_h - 10:
+            self.rect.bottom -= self.speed
+            if ball.isCollidingBot:
+                ball.rect.y -= self.speed
+    
+    def update(self):
+        self.movement()
+        self.outofWIndow()
+            
 pygame.init()
 
 w_w = 1000
@@ -94,6 +181,9 @@ playerGroup.add(Player())
 
 ballGroup = pygame.sprite.GroupSingle()
 ballGroup.add(Ball())
+
+botGroup = pygame.sprite.GroupSingle()
+botGroup.add(Bot())
 
 # OTHER ELEMENTS
 section = pygame.transform.scale_by(pygame.image.load("sprites/section.png"), 1.7)
@@ -114,8 +204,8 @@ pause_text = texts.makeText("PAUSADO", 100, (w_w/2, 100), screen)
 return_text = texts.makeText("Retomar", 35, (return_button.rect.centerx, return_button.rect.bottom + 17), screen)
 
 # GAMESTATES
-home = False
-gameMode_points = True
+home = True
+gameMode_points = False
 in_points = False
 gameMode_survive = False
 in_survive = False
@@ -139,13 +229,13 @@ while True:
         if gameMode_points_button.isClicked() == True:
             home = False
             gameMode_points = True
-            in_points = True     
+            
         if gameMode_survive_button.isClicked() == True:
             home = False
             gameMode_survive = True
-            in_survive = True
     
     if gameMode_points:
+        in_points = True
         screen.fill("black") 
         pause_button.showButton()
         pygame.draw.line(screen, "white", (w_w/2, 0), (w_w/2, w_h), 3)
@@ -153,11 +243,14 @@ while True:
         playerGroup.update()
         ballGroup.draw(screen)
         ballGroup.update()
+        botGroup.draw(screen)
+        botGroup.update()
         if pause_button.isClicked() == True:
             gameMode_points = False
             pause = True
         
     if gameMode_survive:
+        in_survive = True
         screen.fill("black") 
         pause_button.showButton()
         pygame.draw.line(screen, "white", (w_w/2, 0), (w_w/2, w_h), 3)
@@ -165,6 +258,8 @@ while True:
         playerGroup.update()
         ballGroup.draw(screen)
         ballGroup.update()
+        botGroup.draw(screen)
+        botGroup.update()
         if pause_button.isClicked() == True:
             gameMode_survive = False
             pause = True
